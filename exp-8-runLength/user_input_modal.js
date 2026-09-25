96b4245a72ff2aa398e2de3417114ee3ff5ac108
Harishankar Priyadarshi <harishankarpriyadarshi423@gmail.com>
2026-09-25 11:34:24 +0530

    e-8-runLength migration bugs fixed of common user

--- exp-8-runLength/user_input_modal.js
+++ exp-8-runLength/user_input_modal.js
@@ -572,7 +572,7 @@
           localStorage.setItem("vlab_exp8_simulation_report_html", html);
           localStorage.setItem("vlab_exp8_simulation_report_updated_at", updatedAt);
 
-          const activeHash = localStorage.getItem("vlab_exp2_active_user_hash");
+          const activeHash = localStorage.getItem("vlab_exp_active_user_hash");
           if (activeHash) {
             localStorage.setItem(`vlab_exp8_user_${activeHash}_simulation_report_html`, html);
             localStorage.setItem(`vlab_exp8_user_${activeHash}_simulation_report_updated_at`, updatedAt);
